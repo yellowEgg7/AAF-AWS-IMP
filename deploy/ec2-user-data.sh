040d7fa4c825d536bcab8596afb4c5362b86a21a
kamdin84 <u2238149@live.warwick.ac.uk>
2023-04-24 16:16:29 +0100

    added ec2 user data shell script

---
+++ deploy/ec2-user-data.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+# This script is injected into the AWS vm on creation
+# and can be used to provision your VM
+# NB it's run as root, so no need for sudo
+
+# debug logs are here
+readonly logName="/var/log/server-setup.log"
+
+echo "Starting $(date)" | tee -a "${logName}"
+
+echo "Install required tools" | tee -a "${logName}"
+yum install -y \
+    docker \
+    iptraf-ng \
+    htop \
+    tmux \
+    vim \
+    curl \
+    git
+
+# put your own github username here
+echo "Setting up ssh access keys" | tee -a "${logName}"
+curl -s https://github.com/kamdin84.keys | tee -a /home/ec2-user/.ssh/authorized_keys
+
+# add ec2 user to the docker group which allows docket to run without being a super-user
+usermod -aG docker ec2-user
+
+# running docker daemon as a service
+chkconfig docker on
+service docker start
+
+echo "Creating rudimentary web page for debugging this VM" | tee -a "${logName}"
+cat <<EOF >>/home/ec2-user/index.html
+<html>
+    <body>
+        <h1>Welcome Warwick WM145 peeps</h1>
+        <div>We hope you enjoy our debug page</div>
+        <div id="image"><img src="https://placedog.net/500/280" /></div>
+    </body>
+</html>
+EOF
+
+echo "Starting a debug nginx web server on port 8080" | tee -a "${logName}"
+docker run -d \
+    --restart always \
+    -v /home/ec2-user/index.html:/usr/share/nginx/html/index.html:ro \
+    -p 8080:80 \
+    nginx
+
+############################################################
+## 👇👇👇👇👇 application install commands here 👇👇👇👇👇
+
+echo "installing Nodejs using NVM" | tee -a "${logName}"
+curl --silent --location https://rpm.nodesource.com/setup_16.x | bash -
+yum -y install nodejs
+
+echo "installing application" | tee -a "${logName}"
+(cd /home/ec2-user && git clone https://github.com/yellowEgg7/AAF-AWS-IMP/tree/main/01-notebook)
+
+echo "installing deps and starting application $(date)" | tee -a "${logName}"
+(cd /home/ec2-user/week-16-lab/app && npm install && DEBUG=* PORT=80 npm start)

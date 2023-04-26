eef608759c76ccf718cf90a01f88cd2ce461a09f
JJ <118286503+yellowEgg7@users.noreply.github.com>
2023-04-26 18:44:36 +0000

    Added the .env file to handle the sever url changing, modified the main.yml to include the newly created rebuild and teardown scripts

--- 01-notebook/.github/workflows/test.js
+++ 01-notebook/.github/workflows/test.js
@@ -1,34 +1,57 @@
-import { strictEqual } from 'assert';
-import { post, get } from 'axios';
+const axios = require('axios');
+const dotenv = require('dotenv');
 
-const SERVER_URL = process.env.SERVER_URL || 'http://127.0.0.1:8080/';
+dotenv.config();
 
-// describe a group of tests related to the note taking application functionality
-describe('Note taking application functionality', () => {
+const SERVER_URL = process.env.SERVER_URL || 'http://127.0.0.1:8080';
 
-  // new note can be added
-  it('should add a new note', async () => {
-    // send a POST request 
-    const response = await post(SERVER_URL, { note: 'Test note' });
-    strictEqual(response.status, 302, 'Expected status code of 302'); 
-    // send a GET request 
-    const getResponse = await get(SERVER_URL);
-    strictEqual(getResponse.status, 200, 'Expected status code of 200'); // expect successful get request
-    strictEqual(getResponse.data.includes('Test note'), true, 'Expected new note to be in the response'); // expect new note to be in the response
+describe('Note API', () => {
+  let noteId;
+
+  test('should create a new note', async () => {
+    const response = await axios.post(`${SERVER_URL}/notes`, {
+      title: 'Test note',
+      content: 'This is a test note'
+    });
+
+    expect(response.status).toBe(201);
+    expect(response.data.title).toBe('Test note');
+    expect(response.data.content).toBe('This is a test note');
+    noteId = response.data.id;
   });
 
-  // a note can be deleted
-  it('should delete a note', async () => {
-    // send a GET request 
-    const getResponse = await get(SERVER_URL);
-    const noteId = getResponse.data.match(/id="(.+?)"/)[1];
-    // send a GET request 
-    const deleteResponse = await get(`${SERVER_URL}/delete?id=${noteId}`);
-    strictEqual(deleteResponse.status, 302, 'Expected status code of 302'); 
-    // send a GET request 
-    const afterDeleteResponse = await get(SERVER_URL);
-    strictEqual(afterDeleteResponse.status, 200, 'Expected status code of 200'); // expect successful get request
-    strictEqual(afterDeleteResponse.data.includes('Test note'), false, 'Expected deleted note to be removed from response'); // expect deleted note to be removed from response
+  test('should get a list of notes', async () => {
+    const response = await axios.get(`${SERVER_URL}/notes`);
+
+    expect(response.status).toBe(200);
+    expect(response.data.length).toBeGreaterThan(0);
   });
 
+  test('should get a single note', async () => {
+    const response = await axios.get(`${SERVER_URL}/notes/${noteId}`);
+
+    expect(response.status).toBe(200);
+    expect(response.data.id).toBe(noteId);
+  });
+
+  test('should update a note', async () => {
+    const response = await axios.put(`${SERVER_URL}/notes/${noteId}`, {
+      title: 'Updated note',
+      content: 'This note has been updated'
+    });
+
+    expect(response.status).toBe(200);
+    expect(response.data.title).toBe('Updated note');
+    expect(response.data.content).toBe('This note has been updated');
+  });
+
+  test('should delete a note', async () => {
+    const response = await axios.delete(`${SERVER_URL}/notes/${noteId}`);
+
+    expect(response.status).toBe(204);
+
+    // verify that the note has been deleted
+    const getResponse = await axios.get(`${SERVER_URL}/notes/${noteId}`);
+    expect(getResponse.status).toBe(404);
+  });
 });

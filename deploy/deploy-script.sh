7a260b260f9aa0052697540891642e2911f7f96c
kamdin84 <u2238149@live.warwick.ac.uk>
2023-04-28 02:13:55 +0100

    Updated the yml and deploy script so that they are able to put the application online

--- deploy/deploy-script.sh
+++ deploy/deploy-script.sh
@@ -1,23 +1,62 @@
-#!/bin/bash
+readonly STACK_NAME="notes-web-app"
+readonly FILE_DIRECTORY="$PWD"
+readonly TAGS=("ApplicationName=notes-application" "Owner=hl2dm@live.com")
+readonly AWS_DEFAULT_REGION="eu-west-1"
 
-export AWS_REGION=eu-west-2
-export AWS_DEFAULT_REGION=eu-west-2
+# where shall I open ssh access from?
+myIpAddress="$(curl -s https://ifconfig.me/)/32"
+readonly myIpAddress
 
-if ! aws sts get-caller-identity
-then
-    echo >&2 "aws creds not working"
-    exit 2
-fi
+# grab user data script and replace any template vars
+UserDataScript=$(<"${FILE_DIRECTORY}/ec2-user-data-node.sh")
+readonly UserDataScript
 
+echo "Deploying network ${STACK_NAME}"
+# deploy network
+aws cloudformation deploy \
+  --template-file "${FILE_DIRECTORY}/template.yml" \
+  --stack-name "${STACK_NAME}-network" \
+  --capabilities CAPABILITY_IAM \
+  --tags "${TAGS[@]}" \
+  --no-fail-on-empty-changeset \
+  --region "${AWS_DEFAULT_REGION}" \
+  --parameter-overrides \
+      stackName="${STACK_NAME}"
 
-readonly UserDataScript="ec2-user-data.sh"
+# have to wait for VPC  
+aws cloudformation wait \
+  stack-exists \
+  --stack-name "${STACK_NAME}-network"
 
+echo "Finding the current Amazon Linux 2 AMI"
+LINUX2_AMI=$(
+  aws ec2 describe-images \
+    --owners amazon \
+    --filters 'Name=name,Values=amzn2-ami-hvm-2.*.*-x86_64-gp2' 'Name=state,Values=available' \
+    --query 'reverse(sort_by(Images, &CreationDate))[:1].ImageId' \
+    --output text
+)
+readonly LINUX2_AMI
+echo "This is the current Amazon Linux 2 AMI: ${LINUX2_AMI}"
 
-aws cloudformation deploy   \
-    --template-file "template.yml"
-    --stack-name "aaf-networks"  \          \
-    --GroupName "kamdin-JJ" \               \    
-    --capabilities CAPABILITY_IAM           \
-    --no-fail-on-empty-changeset            \
-    --region "eu-west-2"                    \
-    --parameter-overrides UserDataScript=${UserDataScript}
+echo "Deploying server ${STACK_NAME}"
+# deploy server
+aws cloudformation deploy \
+  --template-file "${FILE_DIRECTORY}/ec2-setup.yml" \
+  --stack-name "${STACK_NAME}-server" \
+  --capabilities CAPABILITY_IAM \
+  --tags "${TAGS[@]}" \
+  --no-fail-on-empty-changeset \
+  --region "${AWS_DEFAULT_REGION}" \
+  --parameter-overrides \
+      stackName="${STACK_NAME}" \
+      Linux2Ami="${LINUX2_AMI}" \
+      UserDataScript="${UserDataScript}" \
+      myIpAddress="${myIpAddress}"
+
+aws cloudformation wait \
+  stack-exists \
+  --stack-name "${STACK_NAME}-server"
+
+PUBLICIP=$(aws cloudformation list-exports --query "Exports[?Name=='${STACK_NAME}-server-ip-address'].Value" --output text)
+echo "The EC2 Instance's Public IP is $PUBLICIP"
